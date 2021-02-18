62d512752d68448168493c30b00baf9318db14e8
Martin Fabbri <martin.fabbri.a@gmail.com>
2021-02-18 01:21:21 +0000

    pipeline

--- reproducible-dvc/studentpredictor/config.py
+++ reproducible-dvc/studentpredictor/config.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 class Config:
     RANDOM_SEED = 42
-    ASSETS_PATH = Path("./assets")
+    ASSETS_PATH = Path("./reproducible-dvc/assets")
     ORIGINAL_DATASET_FILE_PATH = ASSETS_PATH / "original_dataset" / "udemy_courses.csv"
     DATASET_PATH = ASSETS_PATH / "data"
     FEATURES_PATH = ASSETS_PATH / "features"
